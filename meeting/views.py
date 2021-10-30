d2544601c972008b2fbe527cd0e5aebf3581e536
HipsterSantos <santoscampos269@gmail.com>
2021-10-30 09:26:47 +0100

    setting up the essetials: in order to get started

--- meeting/views.py
+++ meeting/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse(' welcome to second attemp to finish a django course')
+    return render(request,'meeting/index.html')
     
 def testing(request):
     
